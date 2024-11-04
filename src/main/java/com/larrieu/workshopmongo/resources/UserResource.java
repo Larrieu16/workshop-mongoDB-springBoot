c89c8e3be152734ce7579c5e32b473cb1992a70b
Luisa Larrieu <larrieuestudos@gmail.com>
2024-11-04 08:51:33 -0300

    Inserção e deleção de usuário

--- src/main/java/com/larrieu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/larrieu/workshopmongo/resources/UserResource.java
@@ -1,16 +1,17 @@
 package com.larrieu.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.larrieu.workshopmongo.domain.User;
 import com.larrieu.workshopmongo.dto.UserDTO;
 import com.larrieu.workshopmongo.services.UserService;
@@ -33,6 +34,22 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@PostMapping
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value ="/{id}", method=RequestMethod.DELETE)
+ 	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
+
+
 
 
