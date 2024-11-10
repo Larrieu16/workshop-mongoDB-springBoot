1a1cc074d48278d18b03c2e8a27df9dc5d73fc92
Luisa Larrieu <larrieuestudos@gmail.com>
2024-11-10 08:22:47 -0300

    Consulta com vários critérios

--- src/main/java/com/larrieu/workshopmongo/services/PostService.java
+++ src/main/java/com/larrieu/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.larrieu.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.larrieu.workshopmongo.domain.Post;
-import com.larrieu.workshopmongo.domain.User;
-import com.larrieu.workshopmongo.dto.UserDTO;
 import com.larrieu.workshopmongo.repository.PostRepository;
 import com.larrieu.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +22,11 @@ public class PostService {
 	}
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+				
+		
 	}
 }
